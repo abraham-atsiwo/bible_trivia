6fad087f58b3c7eabfb13b000ec8eb8d1123d4e7
ab30atsiwo <ab30atsiwo@gmail.com>
2023-08-29 22:45:59 -0700

    added Bible Knowledge and Church History

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -1,7 +1,8 @@
 
 import COLOSIANS from "./Colossians/Colosians";
 import ESTHER from "./Esther/Esther";
-
+import CHURCH_HISTORY from "./ChurchHistory/ChurchHistory";
+import GENERAL_BIBLE_KNOWLEDGE from "./GeneralBibleKnowledge/GeneralBibleKnowledge";
 
 
 
@@ -9,7 +10,9 @@ import ESTHER from "./Esther/Esther";
 
 const data = [
     {label: 'Colosians', value: COLOSIANS}, 
-    {label: 'Esther', value: ESTHER}
+    {label: 'Esther', value: ESTHER},
+    {label: 'Church History', value: CHURCH_HISTORY},
+    {label: 'General Bible Knowledge', value: GENERAL_BIBLE_KNOWLEDGE}
 ]
 
-export default data 
+export default GENERAL_BIBLE_KNOWLEDGE
